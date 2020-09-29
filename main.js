0684d742e6fac2b8ab49bb410547a6b3768e90a6
Brian Warner <warner@lothar.com>
2020-09-29 01:31:24 -0700

    hack main.js, closer to exercising what we care about
    Note that is isn't yet correct: we won't really be using Workers in the final
    app, because we need synchronous/blocking calls back up to the dispatch unit.
    A proper demo will start with a C program that uses XS API calls and "host
    functions" to connect the dispatch unit with the snapshotted vat.
    
    Using Workers bypasses the requirement that there be a way to access a
    well-known function in the vat that was loaded from a snapshot, as well as
    providing host functions to the newly-loaded vat. With Workers, `postMessage`
    and `worker.onmessage=` are sufficient.

--- main.js
+++ main.js
@@ -25,34 +25,54 @@ function makePromiseKit() {
 }
 
 export async function main() {
-  let vatWorker = new Worker("vatHost", {
+  let sync = makePromiseKit();
+  let post;
+  function connectToWorker(vatWorker) {
+    vatWorker.onmessage = function (message) {
+      trace("from vatHost: ");
+      traceln(message);
+      sync.resolve();
+      sync = makePromiseKit();
+    };
+    const post = (msg) => vatWorker.postMessage(msg);
+    return { post };
+  }
+
+  let vatWorker1 = new Worker("vatHost", {
     allocation: 6 * 1024,
     stackCount: 64,
     slotCount: 32,
   });
+  post = connectToWorker(vatWorker1);
 
-  let sync = makePromiseKit();
-  vatWorker.onmessage = function (message) {
-    trace("from vatHost: ");
-    traceln(message);
-    sync.resolve();
-    sync = makePromiseKit();
-  };
-  const post = (msg) => vatWorker.postMessage(msg);
 
   const aliceVatSrc = bundleSource("./alice-vat.js");
-
   traceln("setBundle...");
   post(["setBundle", aliceVatSrc]);
   await sync.promise;
-  // snapshot the vatWorker?
 
+  // snapshot the vatWorker
+  const snapshot1 = MAKE_SNAPSHOT(vatWorker1);
+  // stop using vatWorker1
+
+
+  // now pretend that the process has just relaunched, and that we'd saved
+  // snapshot1 in a way that we can reload. Make a new Worker from that
+  // snapshot.
+  const vatWorker2 = LOAD_SNAPSHOT(snapshot1);
+  post = connectToWorker(vatWorker2);
   traceln("deliver...");
   post(["deliver", "message", "slot1", { method: "incr", args: [] }]);
   await sync.promise;
   post(["deliver", "message", "slot1", { method: "incr", args: [] }]);
   await sync.promise;
-  // snapshot the vatWorker?
+  // snapshot the vatWorker
+  const snapshot2 = MAKE_SNAPSHOT(vatWorker2);
+
+
+  // pretend we're relaunching the process again
+  const vatWorker3 = LOAD_SNAPSHOT(snapshot2);
+  post = connectToWorker(vatWorker3);
   post(["deliver", "message", "slot1", { method: "decr", args: [] }]);
   await sync.promise;
   post(["deliver", "message", "slot1", { method: "incr", args: [] }]);
